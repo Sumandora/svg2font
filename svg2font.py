06217f36c1149d1dcf2ec64dbaa3f6d35a58cab9
johannes <johannesmiesenhardt@gmail.com>
2024-08-04 15:49:41 +0200

    Add correct_direction option

--- svg2font.py
+++ svg2font.py
@@ -6,12 +6,14 @@ import svgelements
 
 
 parser = argparse.ArgumentParser("svg2font")
-parser.add_argument("--firstchar", help="The begin of character-range which holds the svgs",
+parser.add_argument("--firstchar", help="The begin of character-range which holds he svgs",
                     type=int, default=0xE000)
 parser.add_argument("--path", help="The path to source svgs from", type=str, required=True)
 parser.add_argument("--font_name", help="The name that the font gets", type=str, required=True)
 parser.add_argument("--cpp_map", help="Should a c++ unordered_map definition be generated?",
                     type=bool, default=False, action=argparse.BooleanOptionalAction)
+parser.add_argument("--correct_direction", help="Should directions be corrected to validate glyphs",
+                    type=str, choices=["no", "auto", "always"], default="no")
 args = parser.parse_args()
 
 f = fontforge.font()
@@ -23,8 +25,9 @@ f.fullname = args.font_name
 # The GUI tells me that TTF prefers when em is a power of 2, so I set it from 1000 to 1024
 f.em = 1024
 
-# FontForge doesn't export unset glyphs, so I just set it to unicode so that any firstchar may be supplied
+# FontForge doesn't export unset glyphs, so set encoding to unicode so that any firstchar is valid
 f.encoding = "UnicodeFull"
+
 
 def width_and_height_from_svg(file):
     svg = svgelements.SVG.parse(file)
@@ -35,15 +38,16 @@ def width_and_height_from_svg(file):
 def add_glyph(idx, file):
     glyph = f.createMappedChar(idx)
 
-    # I don't know what FontForge scales with, because it is definitely not just the width/height of the glyph.
-    # I disable it, because I need to know the width and height of the imported glyph
-    glyph.importOutlines(file, scale=False)
+    # I don't know what FontForge scales with, because it is definitely not just the width/height
+    # of the glyph. I disable it, because I need to know the width and height of the imported glyph
+    glyph.importOutlines(file, scale=False, correctdir=True, removeoverlap=True)
 
     width, height = width_and_height_from_svg(file)
 
     aspect_ratio = width / float(height)
-    # this one would break in theory, because if the glyph is taller than wide then it would over-scale,
-    # but since the width of the glyph is adjusted to match the aspect_ratio, that ensures that it never happens
+    # this one would break in theory, because if the glyph is taller than wide then it would
+    # over-scale, but since the width of the glyph is adjusted to match the aspect_ratio,
+    # that ensures that it never happens
     scale_factor = (f.em / height)
 
     # glyphs are always `f.em`-units tall, so the width is this:
@@ -51,17 +55,25 @@ def add_glyph(idx, file):
 
     # why does the transform function only take matrices
     # the following statements can be combined in theory, but I had some weirdness in FontForge,
-    # when testing these things manually and doing them all at once, the glyph would just be teleported to a random
-    # position, which is especially frustrating because the preview does show it at the correct position
+    # when testing these things manually and doing them all at once, the glyph would just be
+    # teleported to a random position, which is especially frustrating
+    # because the preview does show it at the correct position
     glyph.transform([1, 0, 0, 1, -width / 2, -f.ascent + height / 2])  # move to origin
     glyph.transform([scale_factor, 0, 0, scale_factor, 0, 0])  # scale from origin
 
     glyph.transform([1, 0, 0, 1, 0, f.ascent - f.em / 2])  # center vertically
     glyph.transform([1, 0, 0, 1, glyph_width / 2, 0])  # center horizontally
 
-    # transformations affect the width of the glyph. In the gui there is a checkbox, this doesn't seem to exist
-    # set the width after doing the transforms
+    # transformations affect the width of the glyph. In the gui there is a checkbox, this doesn't
+    # seem to exist in the python api. Just set the width after doing the transforms
     glyph.width = glyph_width
+
+    # While there are several pitfalls when importing random svgs into a font,
+    # one of the easier to fix ones is wrong winding order of paths.
+    if args.correct_direction == "always" or \
+            (args.correct_direction == "auto" and not glyph.selfIntersects()):
+        print("Fixing glyph direction")
+        glyph.correctDirection()
 
 
 # character map for later generating code to map from character to index
